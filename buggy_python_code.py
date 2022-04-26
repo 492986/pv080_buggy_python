0166bd92c02b289ba76680cb37118ad807a009fd
unknown <xoprchal@ad.fi.muni.cz>
2022-04-26 18:56:50 +0200

    trigger dependabot

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -10,6 +10,7 @@ import flask
 def transcode_file(request, filename):
     command = 'ffmpeg -i "{source}" output_file.mpg'.format(source=filename)
     subprocess.call(command, shell=True)  # a bad idea!
+    #fsfsdffstat
 
 
 # Assert statements
